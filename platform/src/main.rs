add49a1f8b8a4f508f965ca536452002a33f8558
Dennis Ranke <dennis.ranke@gmail.com>
2022-02-27 22:09:54 +0100

    fix #2: Crash when drawing zero sized line

--- platform/src/main.rs
+++ platform/src/main.rs
@@ -12,15 +12,15 @@ fn main() -> Result<()> {
 
     println!("Compiling loader module");
     let loader = curlywas::compile_file("src/loader.cwa", curlywas::Options::default())?;
-    File::create("bin/loader.wasm")?.write_all(&loader)?;
+    File::create("bin/loader.wasm")?.write_all(&loader.wasm)?;
 
-    println!("Loader (including base module): {} bytes", loader.len());
+    println!("Loader (including base module): {} bytes", loader.wasm.len());
 
     println!("Compiling platform module");
     let platform = curlywas::compile_file("src/platform.cwa", curlywas::Options::default())?;
     println!("Compressing platform module");
     let platform = uw8_tool::pack(
-        &platform,
+        &platform.wasm,
         &uw8_tool::PackConfig::default().with_compression_level(4),
     )?;
     File::create("bin/platform.uw8")?.write_all(&platform)?;
