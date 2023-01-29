bbfb5eba49dc6df3c72fb15cd1a9813567408161
Dennis Ranke <dennis.ranke@gmail.com>
2023-01-30 00:09:25 +0100

    add back event debouncing in file watcher

--- src/filewatcher.rs
+++ src/filewatcher.rs
@@ -1,23 +1,35 @@
 use anyhow::{anyhow, bail, Result};
-use notify::{Event, EventKind, RecommendedWatcher, Watcher};
-use std::{collections::BTreeSet, path::PathBuf, sync::mpsc};
+use notify_debouncer_mini::{
+    new_debouncer,
+    notify::{self, RecommendedWatcher},
+    DebouncedEvent, DebouncedEventKind, Debouncer,
+};
+use std::{collections::BTreeSet, path::PathBuf, sync::mpsc, time::Duration};
 
 pub struct FileWatcher {
-    watcher: RecommendedWatcher,
+    debouncer: Debouncer<RecommendedWatcher>,
     watched_files: BTreeSet<PathBuf>,
     directories: BTreeSet<PathBuf>,
-    rx: mpsc::Receiver<Event>,
+    rx: mpsc::Receiver<DebouncedEvent>,
 }
 
 impl FileWatcher {
     pub fn new() -> Result<FileWatcher> {
         let (tx, rx) = mpsc::channel();
-        let watcher = notify::recommended_watcher(move |res| match res {
-            Ok(event) => _ = tx.send(event),
-            Err(err) => eprintln!("Error watching for file changes: {err}"),
+        let debouncer = new_debouncer(Duration::from_millis(100), None, move |res| match res {
+            Ok(events) => {
+                for event in events {
+                    let _ = tx.send(event);
+                }
+            }
+            Err(errs) => {
+                for err in errs {
+                    eprintln!("Error watching for file changes: {err}");
+                }
+            }
         })?;
         Ok(FileWatcher {
-            watcher,
+            debouncer,
             watched_files: BTreeSet::new(),
             directories: BTreeSet::new(),
             rx,
@@ -29,7 +41,8 @@ impl FileWatcher {
         let parent = path.parent().ok_or_else(|| anyhow!("File has no parent"))?;
 
         if !self.directories.contains(parent) {
-            self.watcher
+            self.debouncer
+                .watcher()
                 .watch(parent, notify::RecursiveMode::NonRecursive)?;
             self.directories.insert(parent.to_path_buf());
         }
@@ -41,13 +54,11 @@ impl FileWatcher {
     pub fn poll_changed_file(&self) -> Result<Option<PathBuf>> {
         match self.rx.try_recv() {
             Ok(event) => match event.kind {
-                EventKind::Create(_) | EventKind::Modify(_) => {
-                    for path in event.paths {
-                        let handle = same_file::Handle::from_path(&path)?;
-                        for file in &self.watched_files {
-                            if handle == same_file::Handle::from_path(file)? {
-                                return Ok(Some(path));
-                            }
+                DebouncedEventKind::Any => {
+                    let handle = same_file::Handle::from_path(&event.path)?;
+                    for file in &self.watched_files {
+                        if handle == same_file::Handle::from_path(file)? {
+                            return Ok(Some(event.path));
                         }
                     }
                 }
